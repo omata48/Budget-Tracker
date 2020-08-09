e7c27606175d188e6ba7f17b69866a65dfef8a07
Omar <62365988+omata48@users.noreply.github.com>
2020-08-09 18:21:59 -0500

    updates to server, app failing on heroku

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ mongoose.connect("mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/html.js"));
-app.use(require("./routes/api.js"));
+
+app.get("/", (req,res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
