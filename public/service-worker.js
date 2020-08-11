c33d64dc406c80d4cacf2b92239815aee7b326b2
Omar <62365988+omata48@users.noreply.github.com>
2020-08-11 17:17:50 -0500

    Revert "using webpack"
    This reverts commit fee96b12cc0729f49f660be0c40e05bbe034ede6.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,70 +1,58 @@
-var FILES_TO_CACHE = [
-  "/", 
-  "/db.js",
-  "/assets/css/styles.css",
-  
-  "/dist/app.bundle.js",
-  "/dist/manifest.json",
-  
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+const FILES_TO_CACHE = ["/", "/index.html", "index.js"];
 
-];
-
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", evt => {
+self.addEventListener("install", function(evt) {
   evt.waitUntil(
-    caches
-      .open(STATIC_CACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
+  self.skipWaiting();
 });
 
 // activate
-self.addEventListener("activate", evt => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+self.addEventListener("activate", function(evt) {
   evt.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
-      })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cachesToDelete => {
-            return caches.delete(cachesToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
 });
 
 // fetch
-self.addEventListener("fetch", evt => {
-  if (
-    evt.request.method !== "GET" ||
-    !evt.request.url.startsWith(self.location.origin)
-    ) {
-      evt.respondWith(fetch(evt.request));
-    }
-
-  else if (evt.request.url.includes("/api/")) {
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.open(RUNTIME_CACHE).then(cache => {
+      caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
-            cache.put(evt.request, response.clone());
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
             return response;
           })
-          .catch(() => caches.match(evt.request));
-      })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
     );
+
     return;
-  }
-});
+}});
